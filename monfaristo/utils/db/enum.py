8afabb0a47b07140783be202b2dbc245c51eb741
Bugra Sefercik <bugra.sefercik@udemy.com>
2023-12-11 11:58:02 +0300

    remove django app

--- monfaristo/utils/db/enum.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ChoiceEnum(Enum):
-    """Simple Enum that has a choices option"""
-
-    @classmethod
-    def choices(cls):
-        return tuple((prop.name, prop.value) for prop in cls)
